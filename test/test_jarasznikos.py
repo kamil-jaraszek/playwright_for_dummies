c99246e834b41e2573915c47bc0c6a6f7c5bcf72
kamil-jaraszek <kamiljaraszek@proton.me>
2026-05-07 20:26:20 +0200

    add headless=True

--- test/test_jarasznikos.py
+++ test/test_jarasznikos.py
@@ -2,7 +2,7 @@ from playwright.sync_api import sync_playwright
 
 def test_run():
     with sync_playwright() as p:
-        browser = p.chromium.launch(headless=False)
+        browser = p.chromium.launch(headless=True)
         page = browser.new_page()
         page2 = browser.new_page()
 
